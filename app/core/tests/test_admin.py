ed5aef32d0f3a85da1ddc25f69398a8cda2a2f25
jennieewen <jenniferwen84@gmail.com>
2019-03-05 18:49:55 -0600

    Configured postgres db

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,12 +8,12 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@londonappdev.com',
+            email='jxw092220@gmail.com',
             password='password123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='test@londonappdev.com',
+            email='testme@londonappdev.com',
             password='password123',
             name='Test user full name'
         )
@@ -25,21 +25,6 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-
-    def test_user_change_page(self):
-        """Test that the user edit page works"""
-        url = reverse('admin:core_user_change', args=[self.user.id])
-        res = self.client.get(url)
-
-        self.assertEqual(res.status_code, 200)
-
-    def test_create_user_page(self):
-        """Test that the create user page works"""
-        url = reverse('admin:core_user_add')
-        res = self.client.get(url)
-
-        self.assertEqual(res.status_code, 200)
-
 
     def test_user_change_page(self):
         """Test that the user edit page works"""
