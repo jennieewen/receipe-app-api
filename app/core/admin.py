97eef5d7a358f429c8c21ec61d2fab9022cecb01
jennieewen <jenniferwen84@gmail.com>
2019-03-05 22:26:02 -0600

    Re-factored tests and ingredients

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,18 +4,18 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
             (None, {'fields': ('email', 'password')}),
             (_('Personal Info'), {'fields': ('name',)}),
-            (
-            _('Permissions'),
+            (('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
-            ),
+               ),
            (_('Important dates'), {'fields': ('last_login',)})
-         )
+               )
     add_fieldsets = (
            (None, {
                'classes': ('wide',),
@@ -24,5 +24,6 @@ class UserAdmin(BaseUserAdmin):
          )
 
 
-
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+def sample_user(email='test@londonappdev.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -23,12 +29,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email.lower())
 
-
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
@@ -39,3 +43,23 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+
+     def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+
+     def test_ingredient_str(self):
+        """Test the ingredient string respresentation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Avocado'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
